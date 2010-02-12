076f8c87af8112ee90976a90419c191c41287354
Dan Loewenherz <dloewenherz@gmail.com>
2010-02-11 23:19:41 -0800

    add regex example

--- examples/hello.py
+++ examples/hello.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append("..")
+
 from lighthouse import Lighthouse
 from flup.server.fcgi import WSGIServer
 
 def home():
     return "Hello, world"
 
+def regex(*a):
+    return str(a)
+
 urls = (
     ('/', home),
+    ('/(.+)', regex),
 )
 
 WSGIServer(Lighthouse(urls)).run()
